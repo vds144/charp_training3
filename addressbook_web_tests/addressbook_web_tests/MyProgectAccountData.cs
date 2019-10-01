74b7230ccdd33143fc861c51fac8607050d1f46c
Andrei <Andrei@DESKTOP-ES6IN9K>
2019-10-01 23:30:19 +0300

    Задание №4: Сделать всё то же самое, но для контактов

---
+++ addressbook_web_tests/addressbook_web_tests/MyProgectAccountData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgectCreateAccountAddressbookTests
+{
+    class MyProgectAccountData
+    {
+        private string username;
+        private string password;
+
+        public MyProgectAccountData(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+    }
+}
---
+++ addressbook_web_tests/addressbook_web_tests/MyProgectGroupData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProgectCreateAccountAddressbookTests
+{
+    class MyProgectGroupData
+    {
+       // private string name;
+        private string firstname;
+        private string middlename = "";
+        private string lastname = "";
+
+
+
+        public MyProgectGroupData(string firstname)
+            
+            {
+            this.firstname = firstname;
+
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+        public string Middlename
+        {
+            get
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
